74fec01a185e514340a7d7c7d1bf64c6a7e9093b
Alka-agg <37962324+Alka-agg@users.noreply.github.com>
2018-04-02 01:08:41 +0530

    birthday cake candles

---
+++ Birthday.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Birthday {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        int max=ar[0], count=0;
+        for(int i=0; i<n; i++)
+        {
+            if(max<ar[i])
+            {
+                max = ar[i];
+          
+            }
+            
+        }
+        for(int i=0; i<n; i++)
+        {
+            if(ar[i]==max)
+                count++;
+            
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
